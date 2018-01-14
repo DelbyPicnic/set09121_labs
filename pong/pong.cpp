35c2439691ff33ed128188e432e77c7098cfe32c
DelbyPicnic <gordonswan@me.com>
2018-01-14 21:17:24 +0000

    Fixed paddle movement. Fixed ball movement. Added collision detection. TODO: Revise score implementation BUGS: Ball can become trapped inside a paddle, needs to be moved out of the paddle on collision detection.

--- pong/pong.cpp
+++ pong/pong.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace sf;
 using namespace std;
@@ -16,8 +17,22 @@ const int gameWidth = 800;
 const int gameHeight = 600;
 const float paddleSpeed = 400.f;
 
+Vector2f ballVelocity;
+bool server = false;
+
 CircleShape ball;
 RectangleShape paddles[2];
+
+void Reset(){
+    // Reset paddle position
+    paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
+    paddles[1].setPosition(gameWidth - paddleSize.x - 10, gameHeight / 2);
+    // Reset ball position
+    ball.setPosition(gameWidth / 2, gameHeight / 2);
+
+    ballVelocity = {server ? 100.0f : -100.0f, 60.0f};
+}
+
 
 void Load(){
     // Set size and origin of paddles
@@ -28,11 +43,8 @@ void Load(){
     // Set size and origin of ball
     ball.setRadius(ballRadius -3);
     ball.setOrigin(ballRadius / 2, ballRadius / 2);
-    // Reset paddle position
-    paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
-    paddles[1].setPosition(gameWidth - paddleSize.x - 10, gameHeight / 2);
-    // Reset ball position
-    ball.setPosition(gameWidth / 2, gameHeight / 2);
+
+    Reset();
 }
 
 void Update(RenderWindow &window){
@@ -47,21 +59,69 @@ void Update(RenderWindow &window){
             return;
         }
     }
-
     // Close game on escape key
     if (Keyboard::isKeyPressed(Keyboard::Escape)){
         window.close();
     }
 
     // Handle paddle movement
-    float direction = 0.0f;
+    float lp_dir = 0.0f;
+    float rp_dir = 0.0f;
     if(Keyboard::isKeyPressed(controls[0])){
-        direction--;
+        lp_dir--;
     }
     if(Keyboard::isKeyPressed(controls[1])){
-        direction++;
+        lp_dir++;
     }
-    paddles[0].move(0, direction * paddleSpeed * dt);
+    if(Keyboard::isKeyPressed(controls[2])){
+        rp_dir--;
+    }
+    if(Keyboard::isKeyPressed(controls[3])){
+        rp_dir++;
+    }
+
+    paddles[0].move(0, lp_dir * paddleSpeed * dt);
+    paddles[1].move(0, rp_dir * paddleSpeed * dt);
+
+    // Manage ball movement
+    ball.move(ballVelocity * dt);
+    // Check ball collision
+    const float bx = ball.getPosition().x;
+    const float by = ball.getPosition().y;
+
+    
+    if(bx > gameWidth){
+        // Right wall
+        Reset();
+    }else if(bx < 0){
+        // Left wall
+        Reset();
+    }else if(by > gameHeight){
+        // Bottom wall
+        ballVelocity.x *= 1.1f;
+        ballVelocity.y *= -1.1f;
+        ball.move(0, -10);
+    }else if(by < 0){
+        // Top wall
+        ballVelocity.x *= 1.1f;
+        ballVelocity.y *= -1.1f;
+        ball.move(0, 10); 
+    }else if(
+        bx < paddles[0].getPosition().x &&
+        by > paddles[0].getPosition().y - (paddleSize.y * 0.5) &&
+        by < paddles[0].getPosition().y + (paddleSize.y * 0.5)
+    ){
+        // Bounce off left paddle
+        ballVelocity.x *= -1.0f;
+
+    }else if(
+        bx > paddles[1].getPosition().x &&
+        by > paddles[1].getPosition().y - (paddleSize.y * 0.5) &&
+        by < paddles[1].getPosition().y + (paddleSize.y * 0.5)
+    ){
+        // Bounce off right paddle
+        ballVelocity.x *= -1.0f;
+    }
 }
 
 void Render(RenderWindow &window){
